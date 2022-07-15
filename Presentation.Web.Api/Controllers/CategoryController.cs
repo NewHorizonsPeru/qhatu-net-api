47d9aa284db1cba111838a9035e8753b90124c43
Renato De la Rosa <rdelarosa@develovers.com.pe>
2022-07-14 19:00:49 -0500

    Configuracion

--- Presentation.Web.Api/Controllers/CategoryController.cs
+++ Presentation.Web.Api/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using Application.MainModule.IServices;
+﻿using Application.MainModule.DTO;
+using Application.MainModule.IServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation.Web.Api.Controllers
@@ -13,6 +14,44 @@ namespace Presentation.Web.Api.Controllers
         public CategoryController(ICategoryService categoryService)
         {
             _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        [Route("getAll")]
+        public IActionResult GetAll()
+        {
+            return Ok(_categoryService.GetAll());
+        }
+
+        [HttpGet]
+        [Route("getById/{id}")]
+        public IActionResult GetById(int id)
+        {
+            return BadRequest(_categoryService.GetById(id));
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public IActionResult Add(CategoryDto categoryDto)
+        {
+            _categoryService.Add(categoryDto);
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("update")]
+        public IActionResult Update(int id, CategoryDto categoryDto)
+        {
+            _categoryService.Update(id, categoryDto);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("remove")]
+        public IActionResult Remove(int id)
+        {
+            _categoryService.Remove(id);
+            return Ok();
         }
     }
 }
